30ccc829057bdeb854b3b267c068320070348ea0
Francesco Scali <francesco.scali@gmail.com>
2013-07-07 07:26:59 +0000

    make

--- setup.sh
+++ setup.sh
@@ -29,6 +29,7 @@ sudo apt-add-repository -y ppa:cassou/emacs
 sudo apt-get update
 sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
 sudo apt-get install -y python-rope python-ropemacs ipython
+sudo apt-get install -y make
 # git pull and install dotfiles as well
 cd $HOME
 if [ -d ./dotfiles/ ]; then
