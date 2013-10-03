5f365bf384c683a4046719694067ecb469ca30cb
Francesco Scali <francesco.scali@gmail.com>
2013-10-03 12:40:23 +0000

    python notebook

--- setup.sh
+++ setup.sh
@@ -33,6 +33,7 @@ sudo apt-add-repository -y ppa:cassou/emacs
 sudo apt-get update
 sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
 sudo apt-get install -y python-rope python-ropemacs ipython python3 ipython3
+sudo apt-get install ipython-notebook python-matplotlib python-scipy python-pandas python-simpy python-nose
 sudo apt-get install -y make
 # git pull and install dotfiles as well
 cd $HOME
